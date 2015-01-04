c08c70e7c18b516de794961cf22707369b285d6c
Sidhant Panda <sidhantpanda@gmail.com>
2015-01-04 13:28:47 +0530

    modifying to work with list of download

--- download.py
+++ download.py
@@ -1,1 +1,33 @@
 import youtube_dl
+
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+    'logger': MyLogger(),
+    'progress_hooks': [my_hook],
+}
+
+songs = ["https://www.youtube.com/watch?v=Fsj2wdFDmLk",
+		"https://www.youtube.com/watch?v=wfbCmoEGZJk"]
+
+for song in songs:
+	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+		ydl.download([song])
